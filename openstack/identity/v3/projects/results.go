6db84f0bbe93db2733d905e045f47b27977d9f8b
Christopher Parratto <christopher_parratto@comcast.com>
2017-01-25 16:56:09 -0500

    Go formatted some stuff.

---
+++ openstack/identity/v3/projects/results.go
@@ -0,0 +1,48 @@
+package projects
+
+import (
+	"github.com/mitchellh/mapstructure"
+	"github.com/rackspace/gophercloud"
+)
+
+// Project the object to hold a project.
+type Project struct {
+	ID          string `json:"id,omitempty"`
+	IsDomain    bool   `json:"is_domain,omitempty"`
+	Description string `json:"description,omitempty"`
+	DomainID    string `json:"domain_id,omitempty"`
+	Enabled     bool   `json:"enabled,omitempty"`
+	Name        string `json:"name"`
+	ParentID    string `json:"parent_id,omitempty"`
+	Links       Link   `json:"links,omitempty"`
+}
+
+// Link the object to hold a project link.
+type Link struct {
+	Self string `json:"self,omitempty"`
+}
+
+type commonResult struct {
+	gophercloud.Result
+}
+
+// Extract interprets a GetResult, CreateResult or UpdateResult as a concrete Service.
+// An error is returned if the original call or the extraction failed.
+func (r commonResult) Extract() (*Project, error) {
+	if r.Err != nil {
+		return nil, r.Err
+	}
+
+	var res struct {
+		Project `json:"project"`
+	}
+
+	err := mapstructure.Decode(r.Body, &res)
+
+	return &res.Project, err
+}
+
+// CreateResult the object to hold a project link.
+type CreateResult struct {
+	commonResult
+}
---
+++ openstack/identity/v3/projects/urls.go
@@ -0,0 +1,7 @@
+package projects
+
+import "github.com/rackspace/gophercloud"
+
+func listURL(client *gophercloud.ServiceClient) string {
+	return client.ServiceURL("projects")
+}
