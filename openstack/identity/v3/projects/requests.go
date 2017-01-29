79e25dcfef14bec0492521e4e8f84f7115901566
Christopher Parratto <christopher_parratto@comcast.com>
2017-01-29 16:46:00 -0500

    Added method to bind role to group and project.

--- openstack/identity/v3/projects/requests.go
+++ openstack/identity/v3/projects/requests.go
@@ -1,6 +1,7 @@
 package projects
 
 import (
+	"errors"
 	"github.com/rackspace/gophercloud"
 )
 
@@ -29,4 +30,27 @@ func Create(client *gophercloud.ServiceClient, opts CreateOpts) CreateResult {
 	var result CreateResult
 	_, result.Err = client.Post(listURL(client), req, &result.Body, nil)
 	return result
+}
+
+// PairOpts allows you to pair roles, groups, on a project
+type PairOpts struct {
+	ID      string `json:"id"`
+	GroupID string `json:"group_id"`
+	RoleID  string `json:"role_id"`
+}
+
+// Pair creates a relationship between a role, group, and project
+func Pair(client *gophercloud.ServiceClient, opts PairOpts) error {
+	if opts.ID == "" || opts.GroupID == "" || opts.RoleID == "" {
+		return errors.New("Project, Role, and Group ids are required.")
+	}
+
+	reqOpts := &gophercloud.RequestOpts{
+		OkCodes:     []int{204},
+		MoreHeaders: map[string]string{"Content-Type": ""},
+	}
+
+	var result PairResult
+	_, result.Err = client.Put(pairProjectGroupAndRoleURL(client, opts.ID, opts.GroupID, opts.RoleID), nil, nil, reqOpts)
+	return result.Err
 }
